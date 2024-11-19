093f96d567aea45ad774659909ab46b067aeda9b
HUY <vodinhhuy908@gmail.com>
2024-11-19 19:01:24 +0700

    auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/verify-email/{token}', [AuthController::class, 'verifyEmail']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('/change-password', [AuthController::class, 'changePassword']);
+
+Route::get('/admin/users', [AdminController::class, 'getAllUser']);
+Route::post('/admin/addUser', [AdminController::class, 'addUser']);
+Route::put('/admin/updateUser/{id}', [AdminController::class, 'updateUser']);
+Route::delete('/admin/deleteUser/{id}', [AdminController::class, 'deleteUser']);
